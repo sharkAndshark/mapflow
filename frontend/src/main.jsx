c505004fd3641f8612f3d825e1014fd92d8ecf05
Lucas <zhangyijunmetro@hotmail.com>
2026-02-12 12:51:02 +0800

    fix: Remove PublicMap.jsx reference from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import App from './App.jsx';
 import Login from './Login.jsx';
 import Init from './Init.jsx';
 import Preview from './Preview.jsx';
-import PublicMap from './PublicMap.jsx';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
@@ -33,7 +32,6 @@ root.render(
             </ProtectedRoute>
           }
         />
-        <Route path="/tiles/:slug" element={<PublicMap />} />
       </Routes>
     </AuthProvider>
   </BrowserRouter>,
